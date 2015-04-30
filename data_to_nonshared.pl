e34a5eff0daa19d9e2664aaa07a784ef1e9f5532
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-04-30 09:07:50 +0000

    do not omit comments
    Fri Mar 20 18:19:07 CET 2015
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@14879 7cf035df-8472-40d0-8fbe-a733a24d2855

--- data_to_nonshared.pl
+++ data_to_nonshared.pl
@@ -5,23 +5,29 @@ use strict;
 
 while (my $line = <>) {
     chomp $line;
+    my ($shared_first, @shared_rest) = split /\t/, $line;
     my @feats;
-    if ($line =~ /^shared/) {
-        $line =~ s/^.*\|[^ ]* //;
+    if ($shared_first =~ /^shared/) {
+        $shared_first =~ s/^.*\|[^ ]* //;
 #        print STDERR "ERR: $line\n";
-        @feats = split /\s+/, $line;
+        @feats = split /\s+/, $shared_first;
     }
-    $line = <>;
-    chomp $line;
-    $line =~ s/\|.*$//;
-    $line = $line . "| " . (join " ", map {"a=0,".$_} @feats);
-    print $line . "\n";
     
     $line = <>;
     chomp $line;
-    $line =~ s/\|.*$//;
-    $line = $line . "| " . (join " ", map {"a=1,".$_} @feats);
-    print $line . "\n";
+    my ($first, @rest) = split /\t/, $line;
+    $first =~ s/\|.*$//;
+    $first = $first . "| " . (join " ", map {"a=0,".$_} @feats);
+    print join "\t", ($first, @rest, @shared_rest);
+    print "\n";
+    
+    $line = <>;
+    chomp $line;
+    ($first, @rest) = split /\t/, $line;
+    $first =~ s/\|.*$//;
+    $first = $first . "| " . (join " ", map {"a=1,".$_} @feats);
+    print join "\t", ($first, @rest, @shared_rest);
+    print "\n";
     
     $line = <>;
     print "\n";
