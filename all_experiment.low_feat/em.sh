3bd2a424043d02c684b3c06e80961e5a55694af1
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-01-19 11:16:44 +0100

    number iteration with leading zeros

--- all_experiment.low_feat/em.sh
+++ all_experiment.low_feat/em.sh
@@ -7,11 +7,12 @@
 
 epochs=${1:-20}
 passes=10
-for i in `seq 1 $epochs`; do
-    prev_i=$((i-1))
+for i_int in `seq 1 $epochs`; do
+    i=`printf "%03d" $i_int`
     zcat data/train.nonshared.table.gz 'data/iter'$prev_i'.labeled.train.nonshared.table.gz' | cut -f2 --complement | vw -f 'model/iter'$i'.train.nonshared.model' -b 20 --csoaa_ldf mc --loss_function logistic --holdout_off -k --cache_file vw.cache --passes $passes
     zcat data/unlabeled.train.nonshared.table.gz | cut -f2 --complement | vw -t -i 'model/iter'$i'.train.nonshared.model' -r 'tmp/iter'$i'.result.txt' -b 20
     echo "Pasting results to unlabeled.train.nonshared.table.gz"
     zcat data/unlabeled.train.nonshared.table.gz | $ML_FRAMEWORK_DIR/scripts/paste_data_results.pl 'tmp/iter'$i'.result.txt'  | gzip -c > 'data/iter'$i'.labeled.train.nonshared.table.gz'
     passes=1
+    prev_i=$i_str
 done
