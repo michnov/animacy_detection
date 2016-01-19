58f30c97e9565a19899e69cdc433ba4233121c1d
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-01-19 17:27:10 +0100

    adding gauss mu values to the graph

--- gmm_experiment/anim_prob_graph.py
+++ gmm_experiment/anim_prob_graph.py
@@ -7,11 +7,22 @@ X = []
 Y1 = []
 Yn = []
 
+a0_lab_mi = 0
+a1_lab_mi = 0
+a0_unlab_mi = 0
+a1_unlab_mi = 0
+
 for line in sys.stdin:
     line.strip();
     if line == "":
         continue
     if line.startswith("#"):
+        continue
+    if line.startswith("LABELED_GMM_PARAMS"):
+        (a0_lab_mi, a1_lab_mi) = [ int(x.split(",")[0]) for x in line.split("=")[1].split() ]
+        continue
+    if line.startswith("UNLABELED_GMM_PARAMS"):
+        (a0_unlab_mi, a1_unlab_mi) = [ int(x.split(",")[0]) for x in line.split("=")[1].split() ]
         continue
     if len(X) == 0:
         X = line.split()
@@ -24,4 +35,8 @@ for line in sys.stdin:
     Yn.pop(0)
 
 plt.plot(X, Y1, "b-", X, Yn, "r-")
+plt.axvline(a0_lab_mi, color="b", linestyle="--")
+plt.axvline(a1_lab_mi, color="b", linestyle=":")
+plt.axvline(a0_unlab_mi, color="r", linestyle="--")
+plt.axvline(a1_unlab_mi, color="r", linestyle=":")
 plt.savefig('graph.pdf')
