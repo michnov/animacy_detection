e291af536203b99c69c268e6a84da1f48d163b03
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-01-27 13:34:54 +0100

    no need to specify number of bits if loading a model

--- all_experiment.low_feat/em.sh
+++ all_experiment.low_feat/em.sh
@@ -11,7 +11,7 @@ prev_i=000
 for i_int in `seq 1 $epochs`; do
     i=`printf "%03d" $i_int`
     zcat data/train.nonshared.table.gz 'data/iter'$prev_i'.labeled.train.nonshared.table.gz' | cut -f2 --complement | vw -f 'model/iter'$i'.train.nonshared.model' -b 25 --csoaa_ldf mc --loss_function logistic --holdout_off -k --cache_file vw.cache --passes $passes
-    zcat data/unlabeled.train.nonshared.table.gz | cut -f2 --complement | vw -t -i 'model/iter'$i'.train.nonshared.model' -r 'tmp/iter'$i'.result.txt' -b 25
+    zcat data/unlabeled.train.nonshared.table.gz | cut -f2 --complement | vw -t -i 'model/iter'$i'.train.nonshared.model' -r 'tmp/iter'$i'.result.txt'
     echo "Pasting results to unlabeled.train.nonshared.table.gz"
     zcat data/unlabeled.train.nonshared.table.gz | $ML_FRAMEWORK_DIR/scripts/paste_data_results.pl 'tmp/iter'$i'.result.txt'  | gzip -c > 'data/iter'$i'.labeled.train.nonshared.table.gz'
     passes=1
