fcea7b8bb2756d2e50444b00d61ce5b965a137e2
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-01-27 11:27:11 +0100

    number of bits for hashed features extended to 25 to cover most of the features without a conflict

--- gmm_experiment/x_progress.sh
+++ gmm_experiment/x_progress.sh
@@ -14,7 +14,7 @@ for model in model/iter*; do
     file_id=`echo $model | sed 's/^.*iter\([0-9]\+\).*$/\1/' | perl -pe 'chomp'`
     echo -en $file_id'\t'
 
-    zcat tmp/x_progress/test.table.gz | vw -t -i $model -r 'tmp/'$file_id'.result.txt' -b 20
+    zcat tmp/x_progress/test.table.gz | vw -t -i $model -r 'tmp/'$file_id'.result.txt' -b 25
     cat 'tmp/'$file_id'.result.txt' | ./prob_from_results.pl | perl -e 'my @a = <STDIN>; chomp $_ for (@a); print join "\t", @a; print "\n";'
 done
 
